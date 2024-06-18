68059476321e9f7ee715a1864e4dfbad890cdf57
zeze <honourjack95@gmail.com>
2024-06-18 14:03:31 +0100

    zeze edited the admin.py

--- zeze/admin.py
+++ zeze/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+# zeze/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Profile, Task, Tag
+
+admin.site.register(Profile)
+admin.site.register(Task)
+admin.site.register(Tag)
